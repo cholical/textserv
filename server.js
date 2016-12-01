01a9b2a8e99b0754a1ae4ae992611348b690e1c6
cholical <ronding@live.unc.edu>
2016-12-01 16:06:42 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var http = require('http');
+
+var app = express();
+var port = 8080;
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+
+app.post('/api/login', function (req, res) {
+	
+});
+
+app.post('/api/getDashboard', function (req, res) {
+	
+});
+
+app.post('/api/createList', function (req, res) {
+	
+});
+
+app.post('/api/deleteList', function (req, res) {
+	
+});
+
+app.post('/api/openList', function (req, res) {
+	
+});
+
+app.post('/api/addPerson', function (req, res) {
+	
+});
+
+app.post('/api/removePerson', function (req, res) {
+	
+});
+
+app.post('/api/sendMessage', function (req, res) {
+	
+});
+
+app.post('/api/getMessages', function (req, res) {
+	
+});
+
+app.post('/api/logout', function (req, res) {
+	
+});
+
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(port, function () {
+	console.log("Server running on port " + port);
+});
